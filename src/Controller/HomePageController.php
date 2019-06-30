ba55d5fefc5d1b93f4274492f0ed9b2c8684b314
Ousmane Aymérou BA <ousmane.aymerou.ba@efrei.net>
2019-06-30 14:32:35 +0200

    Version 1 with bootstrap navbar

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use \Symfony\Component\HttpFoundation\Response;
+use Twig\Environment ;
+//use Symfony\Component\Routing\Annotation\Route;
+//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomePageController //extends AbstractController
+{
+
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    public function __construct(Environment $twig){
+        $this -> twig = $twig ;
+    }
+
+    public function wellcoming():Response
+    {
+
+        return new Response($this->twig->render('Home/homepage.html.twig'));
+
+    }
+}
