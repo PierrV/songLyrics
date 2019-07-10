d53bb00102e266e7b917334c048aef367a4375af
Zbeul <pierre.v691@gmail.com>
2019-07-11 00:07:37 +0200

    Page d'erreur

--- src/Controller/ElasticsearchController.php
+++ src/Controller/ElasticsearchController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Repository\SongRepository;
 use Elasticsearch\ClientBuilder;
+use Elasticsearch\Common\Exceptions\ClientErrorResponseException;
+use Elasticsearch\Common\Exceptions\Curl\CouldNotConnectToHost;
+use Elasticsearch\Common\Exceptions\NoNodesAvailableException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,12 +19,6 @@ class ElasticsearchController extends AbstractController
     public function __construct()
     {
         $this->hosts = ['localhost:9200'];
-
-        $this->client = ClientBuilder::create()
-            ->setHosts($this->hosts)
-            ->build();
-
-        $this->repository = new SongRepository($this->client);
     }
 
     /**
@@ -29,10 +26,32 @@ class ElasticsearchController extends AbstractController
      * @param $lyric
      */
     public function search($lyric){
+        $this->open();
         $value = htmlentities($lyric);
-        $response = $this->repository->search($value);
-        var_dump($response['hits']['hits']);
-        die();
+        try {
+            $response = $this->repository->search($value);
+        } catch (NoNodesAvailableException $no_node_alive){
+            $this->close();
+            return $this->render('erreur.html.twig', ['erreur1' => "THE DATABASE", 'erreur2' => "WAS NOT FOUND"]);
+        }
+        $this->close();
+        return $this->render('Home/homepage.html.twig');
+    }
+
+    public function open(){
+        try {
+            $this->client = ClientBuilder::create()
+                ->setHosts($this->hosts)
+                ->build();
+        } catch (CouldNotConnectToHost $host_error){
+            return $this->render('erreur.html.twig', ['erreur1' => "CONNECTION", 'erreur2' => "HAS FAILED"]);
+        }
+
+        $this->repository = new SongRepository($this->client);
+    }
+
+    public function close(){
+        $this->client = null;
     }
 
 
